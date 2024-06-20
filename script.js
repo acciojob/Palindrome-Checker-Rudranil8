7fa042b1a0dcba70d9819da6eafb55e022efa618
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-06-20 22:09:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+	let a=str.toLowerCase();
+	let b="";
+	for(let i=a.length-1; i>=0; i--){
+		b=b+a[i];
+	}
+	if(a===b){
+		document.write(true);
+	}
 }
 module.exports = palindrome
